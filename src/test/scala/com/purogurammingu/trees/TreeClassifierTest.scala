3c4b4f1850dcd9728e0710355ca7b261a2acc716
Mateusz Dymczyk <dymczyk@gmail.com>
2016-02-11 21:26:11 +0900

    Add a tree classifier

---
+++ src/test/scala/com/purogurammingu/trees/TreeClassifierTest.scala
@@ -0,0 +1,27 @@
+package com.purogurammingu.trees
+
+import com.purogurammingu.dataset.{Observation, Dataset}
+import org.scalatest.FunSuite
+
+/**
+  * Created by mateusz on 2/11/16.
+  */
+class TreeClassifierTest extends FunSuite {
+
+  test("Should classify") {
+    val ds = new Dataset(
+      Seq(
+        new Observation(Vector(1, 1), "y"),
+        new Observation(Vector(1, 1), "y"),
+        new Observation(Vector(1, 0), "n"),
+        new Observation(Vector(0, 1), "n"),
+        new Observation(Vector(0, 1), "n")
+      )
+    )
+    val classifier = new TreeClassifier(ds)
+    assert(classifier.classify(Vector(1, 1)) == "y")
+    assert(classifier.classify(Vector(0, 1)) == "n")
+    assert(classifier.classify(Vector(0, 0)) == "n")
+  }
+
+}
