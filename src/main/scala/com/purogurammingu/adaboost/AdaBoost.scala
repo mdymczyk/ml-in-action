ad97a87f90ac22cfd6528079057ea931423b5b9c
Mateusz Dymczyk <dymczyk@gmail.com>
2016-03-06 16:10:33 +0900

    Started writing an ada boost trainer

--- src/main/scala/com/purogurammingu/adaboost/AdaBoost.scala
+++ src/main/scala/com/purogurammingu/adaboost/AdaBoost.scala
@@ -1,6 +1,6 @@
 package com.purogurammingu.adaboost
 
-import breeze.linalg.{max, min, DenseMatrix, DenseVector}
+import breeze.linalg._
 import com.purogurammingu.adaboost.AdaBoost.ThresholdInequality.ThresholdInequality
 
 import scala.collection.mutable
@@ -32,25 +32,25 @@ object AdaBoost {
   }
 
   def buildStump(dataMatrix: DenseMatrix[Double], labels: DenseVector[Double], D: DenseVector[Double]) = {
-    val m,n = dataMatrix.cols, dataMatrix.rows
+    val m, n = dataMatrix.cols, dataMatrix.rows
     val steps = 10
     val bestStump = mutable.Map[String, Any]()
     var bestClassEst = DenseVector.zeros[Double](m)
     var minError = Double.PositiveInfinity
 
-    for(i <- 0 until n) {
-      val rangeMin = min(dataMatrix(::,i))
-      val rangeMax = max(dataMatrix(::,i))
+    for (i <- 0 until n) {
+      val rangeMin = min(dataMatrix(::, i))
+      val rangeMax = max(dataMatrix(::, i))
       val stepSize = (rangeMax - rangeMin) / steps
-      for(j <- -1 to steps) {
-        for(inequal <- ThresholdInequality.values) {
+      for (j <- -1 to steps) {
+        for (inequal <- ThresholdInequality.values) {
           val threshVal = (rangeMin + j) * stepSize
           val predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
           val errArr = DenseVector.ones[Double](m)
           val array = predictedVals :== labels
           // TODO can this be updated in a better way??
           array.keySet.foreach(i => errArr.update(i, 0))
-          val weightedError = D.t*errArr
+          val weightedError = D.t * errArr
           if (weightedError < minError) {
             minError = weightedError
             bestClassEst = predictedVals.copy
@@ -60,7 +60,25 @@ object AdaBoost {
           }
         }
       }
-      (bestStump, minError, bestClassEst)
+    }
+    (bestStump, minError, bestClassEst)
+  }
+
+  def adaBoostTrainDS(dataMatrix: DenseMatrix[Double], labels: DenseVector[Double], numIt: Int = 40) = {
+    var weakClassArr = mutable.Buffer[mutable.Map[String, Any]]()
+    val m = dataMatrix.cols
+    var D: DenseVector[Double] = DenseVector.ones[Double](m) / m.toDouble
+    val aggClassEst = DenseVector.zeros[Double](m)
+    for (i <- 0 until numIt) {
+      val (bestStump, error, classEst) = buildStump(dataMatrix, labels, D)
+      val alpha = 0.5 * math.log((1.0 - error) / max(error, 1e-16))
+      bestStump.put("alpha", alpha)
+      weakClassArr.+=(bestStump)
+      val expon = (-1 * alpha * labels.t) * classEst
+      D = D * expon.map(math.exp)
+      D = D/sum(D)
+      aggClassEst += alpha*classEst
+      // TODO finish (ADA BOOST chapter)
     }
   }
 
